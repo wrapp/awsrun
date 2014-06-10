f26b2927cade080dc9f527bd45f622ef2b6d41d5
Eskil Andreen <eskil.andreen@gmail.com>
2014-06-10 14:04:34 +0200

    Add awsrun, setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='awsrun',
+    version='0.9',
+    scripts = [
+        'awsrun'
+    ],
+    author='Eskil Andreen',
+    author_email='eskil@wrapp.com',
+    url='https://github.com/wrapp/awsrun',
+)
